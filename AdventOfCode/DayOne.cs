e42a39a48548e8e1c669d536f658558c5cb32d49
Jacob Honoré <jho@blackstoneone.net>
2019-12-01 20:25:05 +0100

    Refactor day one into its own class.

---
+++ AdventOfCode/DayOne.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AdventOfCode
+{
+	class DayOne
+	{
+		public int CalculateSumOfFuel()
+		{
+			var lines = File.ReadLines("Input/DayOne.txt");
+			//var lines = new int[] { 100756 };
+			var sumOfFuel = 0;
+			foreach (var line in lines)
+			{
+				int moduleMass = Convert.ToInt32(line);
+				int moduleFuel = CalculateFuel(moduleMass);
+				var currentFuel = CalculateFuel(moduleFuel);
+				while (currentFuel > 0)
+				{
+					moduleFuel += currentFuel;
+					currentFuel = CalculateFuel(currentFuel);
+				}
+				sumOfFuel += moduleFuel;
+			}
+			return sumOfFuel;
+		}
+		public int CalculateFuel(int mass)
+		{
+			return Convert.ToInt32(Math.Floor(mass / 3.0)) - 2;
+		}
+	}
+}
